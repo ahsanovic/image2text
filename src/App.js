b8810344aa31c59bbd89bb5e968d8050e4e1bc34
ahsanovic <ahsanovic_7@yahoo.com>
2022-03-09 10:35:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState, useCallback } from 'react';
 import './App.css';
+import {createWorker} from 'tesseract.js'
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [textResult, setTextResult] = useState('')
+
+  const worker = createWorker()
+  const convertImageToText = useCallback(async () => {
+    if (!selectedImage) return
+    await worker.load()
+    await worker.loadLanguage('ind')
+    await worker.initialize('ind')
+    const {data} = await worker.recognize(selectedImage)
+    setTextResult(data.text)
+    await worker.terminate();
+  }, [worker, selectedImage])
+
+  useEffect(() => {
+    convertImageToText()
+  }, [selectedImage, convertImageToText])
+
+  const handleChangeImg = (e) => {
+    if (e.target.files[0]) {
+      setSelectedImage(e.target.files[0])
+    } else {
+      setSelectedImage(null)
+      setTextResult('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Image2Text Conversion</h1>
+      <div className='input-wrapper'>
+        <label htmlFor='upload'>Upload Image</label>
+        <input type='file' id='upload' accept='image/*' onChange={handleChangeImg} />
+      </div>
+
+      <div className='result'>
+        {selectedImage && (
+          <div className='box-image'>
+            <img src={URL.createObjectURL(selectedImage)} alt='thumbnail' />
+          </div>
+        )}
+
+        {textResult && (            
+          <div className='box-p'>
+            <p>{textResult}</p>
+          </div>            
+        )}
+      </div>
     </div>
   );
 }
